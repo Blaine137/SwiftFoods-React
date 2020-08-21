f9c115592770058f3427f07445f9f7b42de28e0a
blaine young <blaine.young3553@gmail.com>
2020-08-20 21:26:49 -0400

    added content to the about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import SubscribeForm from '../SubscribeForm/SubscribeForm';
 import './About.scss';
 
 const About = (props) => {
         return(
-            <div>
-                 <h2>About</h2>
-            </div>
+            <Fragment>
+                     <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <h3 class="text-center mt-5 mb-5">About Us!</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="row row-content align-items-center">
+                            <div class="col-6 ">
+                                <h4 class="font-weight-bold">Our Mission</h4>
+                            </div>
+                            <div class="col text-left ">
+                                <p class="w-55 ml-auto">We strive to keep our customers fed with little to no effort. if you are not satisfied with our product, we will refund your money!</p>
+                            </div>
+                        </div>
+                        <div class="row row-content align-items-center">
+                            <div class="col text-left">
+                                <p class="w-55 mr-auto">Here at Swift Foods, we provide the highest quality food. Our food contains less sugar, less processes, and more nutrients that is ready in three minutes!</p>
+                            </div>
+                            <div class="col-6 text-right">
+                                <h4 class="font-weight-bold">Our Food</h4>
+                            </div>
+                        </div>
+                        <div class="row row-content align-items-center">
+                            <div class="col-6">
+                                <h4 class="font-weight-bold">Why Swift Foods?</h4>
+                            </div>
+                            <div class="col text-left">
+                                <p class=" w-55 ml-auto ">When it comes time to eat, you have many options. Foods offer convenience, nutrition, and cravings. At Swift Foods, we offer all three for a prive you cannot beat!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <SubscribeForm/>
+            </Fragment>
         );
 };
 
